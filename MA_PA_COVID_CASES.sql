8f973e5877cb2aa58950d7c9a77837b400f4d49c
vtrieu <vanna.trieu@gmail.com>
2020-10-12 17:15:07 -0400

    Create MA_PA_COVID_CASES.sql
    Created new file

---
+++ MA_PA_COVID_CASES.sql
@@ -0,0 +1,8 @@
+--CTAS
+CREATE OR REPLACE VIEW MA_PA_COVID_CASES AS 
+(
+select country_region, province_state, county, fips, date, case_type
+from "COVID19_BY_STARSCHEMA"."PUBLIC"."JHU_COVID_19"
+where country_region = 'United States' and case_type = 'Confirmed' and province_state in ('Massachusetts','Pennsylvania') and county is not null and fips is not null
+order by date
+);
