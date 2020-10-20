6b4efd48409ce608fb64f53c884b0d17e7bbc7d6
Vanna Trieu <vanna.trieu@gmail.com>
2020-10-20 13:52:35 -0400

    created sql for CA covid cases

---
+++ ca_covid_cases.sql
@@ -0,0 +1,9 @@
+--CTAS to create ma_covid_cases view
+--add cases column
+CREATE OR REPLACE VIEW ca_covid_cases AS 
+(
+select country_region, province_state, county, fips, date, case_type
+from "COVID19_BY_STARSCHEMA"."PUBLIC"."JHU_COVID_19"
+where country_region = 'United States' and case_type = 'Confirmed' and province_state in ('California') and county is not null and fips is not null
+order by date
+);
