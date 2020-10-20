5fff02962ed56a020fd664b5c2b69590e8ee55f5
Vanna Trieu <vanna.trieu@gmail.com>
2020-10-20 13:47:57 -0400

    removed cases

--- ma_covid_cases.sql
+++ ma_covid_cases.sql
@@ -2,7 +2,7 @@
 --add cases column
 CREATE OR REPLACE VIEW ma_covid_cases AS 
 (
-select country_region, province_state, county, fips, date, case_type, cases
+select country_region, province_state, county, fips, date, case_type
 from "COVID19_BY_STARSCHEMA"."PUBLIC"."JHU_COVID_19"
 where country_region = 'United States' and case_type = 'Confirmed' and province_state in ('Massachusetts') and county is not null and fips is not null
 order by date
